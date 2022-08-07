2f189df9abc52d25dc5b273bbb2f3a856be73463
Gabriela Florêncio <perflogabi@gmail.com>
2022-08-07 11:23:20 -0300

    finalizado

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -1,6 +1,15 @@
-function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
 }
+
+
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
